83d8533dd69fb373b4c4996e978fb7a07e9acd94
Juan Sanzone OLX <juan.sanzone@olx.com>
2015-04-14 23:37:08 -0300

    Console NPM Cli

---
+++ cli/README.md
@@ -0,0 +1,34 @@
+# Command Line Tool for OLX Football Tournament
+
+Check OLX Football Tournament Stats from your Console !
+
+## How to Install ?
+
+```
+> sudo npm install -g olxcli
+```
+
+## How to use?
+
+### Show Teams Table
+
+``` 
+> olxcli
+```
+
+### Show Goals Table (Players score)
+
+```
+> olxcli --goals
+```
+
+### Update Remote Database
+This method will get the latest results from Tournament WebSite and update the remote Database with recent values. Any client can run this command after each match in order to update the remote database and replicate the last changes of each client instantly.
+
+```
+> olxcli --update
+```
+
+## Any idea? =)
+
+juan.sanzone@gmail.com
---
+++ cli/index.js
@@ -0,0 +1,152 @@
+#!/usr/bin/env node
+
+var program   = require('commander');
+var http      = require('http');
+var cliTable  = require('cli-table');
+
+program
+  .version('0.0.1')
+  .usage('[options]')
+  .option('-g, --goals [goals]', 'Goals table')
+  .option('-u, --update [update]', 'Update Remote Database')
+  .parse(process.argv);
+
+if (program.goals) 
+{
+  var options = {
+    hostname: 'emitter.pasting.io',
+    port: 80,
+    path: '/?goals=1',
+    method: 'GET'
+  };
+
+  var data = '';
+  var req = http.request(options, function(res) 
+  {
+    res.on('data', function(response) 
+    {
+      data += response; 
+    });
+
+    res.on('end', function() {
+
+      var jsonObj = JSON.parse(data);
+      var goleatorsLimit = 9;
+
+      var table = new cliTable({
+          head: [ "#",'Player', '#Goals', "Team Name"]
+      });
+
+      for (var i = 0; i <= goleatorsLimit; i++) {
+        table.push(
+          [
+            i+1, 
+            jsonObj['Goleador'][i].cell_text, 
+            jsonObj['Goles'][i].cell_text,
+            jsonObj['Equipo'][i].cell_text
+          ]
+        );
+      };
+
+      console.log(table.toString());
+
+    });
+
+  });
+
+  req.on('error', function(e) {
+    process.stdout.write('Error: OLX Football CLI: ' + e.message);
+    console.log('Error: OLX Football CLI: ' + e.message);
+  });
+
+  req.end();
+
+} else {
+
+  if (program.update) 
+  {
+    var options = {
+      hostname: 'emitter.pasting.io',
+      port: 80,
+      path: '/data/do.php',
+      method: 'GET'
+    };
+
+    console.log('Updating..');
+
+    var req = http.request(options, function(res) 
+    {
+      res.on('data', function (response) 
+      {
+        console.log('Updating...');
+      });
+      res.on('end', function() 
+      {
+        console.log('Updated !');
+      });
+    });
+
+    req.on('error', function(e) {
+      process.stdout.write('Error: OLX Football CLI: ' + e.message);
+      console.log('Error: OLX Football CLI: ' + e.message);
+    });
+
+    req.end();
+
+  } else {
+
+    var options = {
+      hostname: 'emitter.pasting.io',
+      port: 80,
+      path: '/?table=1',
+      method: 'GET'
+    };
+
+    var req = http.request(options, function(res) 
+    {
+      var data = '';
+      res.on('data', function(response) 
+      {
+        data += response;
+      });
+
+      res.on('end', function () 
+      {
+        var jsonObj = JSON.parse(data);
+        var teamCount = Object.keys(jsonObj['Equipo']).length;
+
+        var table = new cliTable({
+            head: ["#",'Team', 'Points', "PJ", "G", "E", "P", "GF", "GC", "DF"]
+        });
+
+        for (var i = 0; i <= (teamCount - 1); i++) {
+          table.push(
+            [
+              i+1,
+              jsonObj['Equipo'][i].cell_text, 
+              jsonObj['Puntos'][i].cell_text,
+              jsonObj['PJ'][i].cell_text,
+              jsonObj['G'][i].cell_text,
+              jsonObj['E'][i].cell_text,
+              jsonObj['P'][i].cell_text,
+              jsonObj['GF'][i].cell_text,
+              jsonObj['GC'][i].cell_text,
+              jsonObj['DF'][i].cell_text
+            ]
+          );
+        };
+
+        console.log(table.toString());
+      });
+    });
+
+    req.on('error', function(e) {
+      process.stdout.write('Error: OLX Football CLI: ' + e.message);
+      console.log('Error: OLX Football CLI: ' + e.message);
+    });
+
+    req.end();
+
+  }
+
+}
